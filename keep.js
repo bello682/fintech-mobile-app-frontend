98f5ed8a00101c5cb2b9a64ebc34aa7b5c61c547
bello682 <belloadetayo14@gmail.com>
2025-06-27 17:14:14 +0100

    updated env

--- keep.js
+++ keep.js
@@ -50,3 +50,13 @@
 //     }
 //   }
 // }
+
+// ================================================================================================================================//
+
+// to generate a apk for my mobile app testing
+// 1. login to the eas with the command `eas login`
+// 2. run the command `eas build --profile preview --platform android`
+// 3. wait for the build to complete
+// 4. download the apk from the link provided in the terminal
+// 5. install the apk on your android device
+// 6. run the app on your android device
